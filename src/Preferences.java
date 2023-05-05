7745ae8bbf0e56fe9b3fc1cf621df36828630d68
justmedev <ilja.ragnar@gmail.com>
2023-05-05 23:11:29 +0200

    add method which can make tables of any size and call it in render

--- src/Preferences.java
+++ src/Preferences.java
@@ -1,3 +1,4 @@
 public class Preferences {
     public final static boolean COMPACT_FIELD = false;
+    public final static boolean DEBUG_FIELD = false;
 }
--- src/Renderer.java
+++ src/Renderer.java
@@ -5,40 +5,62 @@ public class Renderer {
     private String floor = "-";
 
     public void render(Piece[][] field) {
+        char[][] matrix = new char[4][4];
+
+        for (int rowIndex = 0; rowIndex < matrix.length; rowIndex++) {
+            if (rowIndex == 0) {
+                matrix[0] = new char[]{
+                        ' ',
+                        'A',
+                        'B',
+                        'C',
+                };
+                continue;
+            }
+
+            for (int index = 0; index < matrix[rowIndex].length; index++) {
+                if (Preferences.DEBUG_FIELD && index != 0) {
+                    System.out.printf("field[%d][%d] = %c%n", rowIndex - 1, index - 1, field[rowIndex - 1][index - 1].getSymbol());
+                }
+
+                matrix[rowIndex][index] = index != 0 ? field[rowIndex - 1][index - 1].getSymbol() : (char) (rowIndex + '0');
+            }
+        }
+
+        String table = makeTable(matrix, 3);
+        System.out.println(table);
+    }
+
+    public String makeTable(char[][] tableMatrix, int innerWidth) {
         StringBuilder sb = new StringBuilder();
 
-        int width = field.length * 3 + 2;
+        int fullWidth = tableMatrix[0].length * innerWidth + tableMatrix[0].length - 1;
 
-        sb.append(corner);
-        sb.append(ceiling.repeat(width));
-        sb.append(corner);
-        sb.append('\n');
+        makeFloor(sb, fullWidth);
 
-        for (int i = 0; i < field.length; i++) {
-            Piece[] pieces = field[i];
+        for (int i = 0; i < tableMatrix.length; i++) {
+            char[] cols = tableMatrix[i];
             sb.append(wall);
 
-            for (Piece piece : pieces) {
+            for (char col : cols) {
                 sb.append(' ');
-                sb.append(piece.getSymbol());
+                sb.append(col);
                 sb.append(' ');
                 sb.append(wall);
             }
 
             sb.append('\n');
-            if (i + 1 != field.length) {
-                for (int j =  0; j < width + 2; j++) {
-                    if (j % 4 == 0) sb.append(corner);
-                    else sb.append(floor);
-                }
-                sb.append('\n');
-            }
+            makeFloor(sb, fullWidth);
         }
 
-        sb.append(corner);
-        sb.append(floor.repeat(width));
-        sb.append(corner);
+        return sb.toString();
+    }
 
-        System.out.println(sb);
+    private void makeFloor(StringBuilder sb, int fullWidth) {
+        for (int j = 0; j < fullWidth + 2; j++) {
+            if (j % 4 == 0) sb.append(corner);
+            else sb.append(floor);
+        }
+        sb.append('\n');
     }
 }
