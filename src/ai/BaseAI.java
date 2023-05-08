fc7ca8482517cfe31f37a23aecd070428252267d
justmedev <ilja.ragnar@gmail.com>
2023-05-08 09:32:58 +0200

    create files for ai

--- src/ai/BaseAI.java
+++ src/ai/BaseAI.java
@@ -1,4 +1,9 @@
 package ai;
 
-public class BaseAI {
+import game.Piece;
+
+public abstract class BaseAI {
+    public abstract void initialize(Piece piece);
+    public abstract void nextMove(Piece[] field, int tick);
+
 }
--- src/ai/RandomAI.java
+++ src/ai/RandomAI.java
@@ -1,4 +1,22 @@
 package ai;
 
-public class RandomAI {
+import engine.Logger;
+import game.Piece;
+
+public class RandomAI extends BaseAI {
+    private final static Logger logger = new Logger("RandomAI");
+    public Piece piece = Piece.EMPTY;
+
+    @Override
+    public void initialize(Piece piece) {
+        this.piece = piece;
+    }
+
+    @Override
+    public void nextMove(Piece[] field, int tick) {
+        if (this.piece == Piece.EMPTY) {
+            logger.err("AI init failed!");
+            System.exit(1);
+        }
+    }
 }
