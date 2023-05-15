e0b496e997b4cdddf89ace7b0052baa054e76ac0
justmedev <ilja.ragnar@gmail.com>
2023-05-15 18:15:08 +0200

    fix: minimax ai now fully works

--- src/ai/MiniMaxAI.java
+++ src/ai/MiniMaxAI.java
@@ -32,9 +32,8 @@ public class MiniMaxAI extends BaseAI {
 
     @Override
     public ChosenField nextMove(Field field, int tick) {
-        int score = miniMax(field.field, game.computer, 0);
-        logger.infof("AI nextMove calculated score: %s", score);
-        return nextAIMove;
+        MiniMaxResult miniMax = miniMax2(field.field, game.computer, getAllEmptyPositions(field.field).size());
+        return new ChosenField(miniMax.row, miniMax.col);
     }
 
     public ArrayList<ChosenField> getAllEmptyPositions(Piece[][] field) {
@@ -48,80 +47,31 @@ public class MiniMaxAI extends BaseAI {
         return empty;
     }
 
-    public int miniMax(Piece[][] fieldState, Piece player, int depth) {
-        // check if we reached search depth or if game end state is reached and return score
-        if (depth >= maxDepth || GameState.checkIfWon(fieldState)) {
-            return calculateScore(fieldState);
+    record MiniMaxResult(int row, int col, int score) {}
+
+    public MiniMaxResult miniMax2(Piece[][] fieldState, Piece player, int depth) {
+        MiniMaxResult best = new MiniMaxResult(-1, -1, Integer.MAX_VALUE * (player == game.player ? 1 : -1));
+
+        if (depth == 0 || GameState.checkIfWon(fieldState)) {
+            return new MiniMaxResult(-1, -1, calculateScore(fieldState));
         }
 
-        int maxScore = 0;
-        int minScore = 0;
+        for (ChosenField emptyPos : getAllEmptyPositions(fieldState)) {
+            int row = emptyPos.row();
+            int col = emptyPos.col();
 
-        ArrayList<Integer> scores = new ArrayList<>();
-        ArrayList<ChosenField> moves = new ArrayList<>();
-        ArrayList<ChosenField> availablePositions = getAllEmptyPositions(game.field.field);
+            fieldState[row][col] = player;
+            MiniMaxResult score = miniMax2(fieldState, player.getOther(), depth - 1);
+            fieldState[row][col] = Piece.EMPTY;
 
-        for (ChosenField availablePosition : availablePositions) {
-            Piece[][] possibleField = game.field.field.clone();
-
-            possibleField[availablePosition.row()][availablePosition.col()] = game.computer;
-            scores.add(miniMax(possibleField, game.player, depth + 1));
-
-            possibleField[availablePosition.row()][availablePosition.col()] = Piece.EMPTY;
-            moves.add(availablePosition);
+            MiniMaxResult newScore = new MiniMaxResult(row, col, score.score);
+            if (player == game.computer) {
+                if (newScore.score > best.score()) best = newScore;
+            } else {
+                if (newScore.score < best.score()) best = newScore;
+            }
         }
 
-        nextAIMove = moves.get(0);
-        if (player == game.player) {
-            maxScore = scores.get(0);
-
-            for (int score : scores) {
-                if (score > maxScore) {
-                    maxScore = scores.get(score);
-                    nextAIMove = moves.get(score);
-                }
-            }
-
-            return maxScore;
-        } else {
-            minScore = scores.get(0);
-
-            for (int score : scores) {
-                if (score < minScore) {
-                    minScore = scores.get(score);
-                    nextAIMove = moves.get(score);
-                }
-            }
-
-            return minScore;
-        }
-        /*
-        var scores = []; //keeps track of score of current board state
-        var moves = []; //keeps track of all available moves of current board state.
-        var opponent = player == "X" ? "O" : "X";
-        var successors = get_available_moves(state); //get's all the available moves from a given board.
-
-        //If a human is playing then we check to see what moves it will take to win the game
-        if (player == "X") {
-            AI_MOVE = moves[0]; //set AI_Move to the first move
-            maxScore = scores[0]; //set max score to the first score
-            for (var s in scores) { //iterate through all the scores
-                if (scores[s] > maxScore) { //looks for all scores then is greater then max
-                    maxScore = scores[s]; //set maxScore to that score
-                    AI_MOVE = moves[s]; //set AI_Move to the best possible move
-                }
-            }
-            return maxScore; //return maxScore
-        } else {
-            AI_MOVE = moves[0]; //Set ai_move and minScore to the first move and score
-            minScore = scores[0];
-            for (var s in scores) {
-                if (scores[s] < minScore) {
-                    minScore = scores[s];
-                    AI_MOVE = moves[s];
-                }
-            }
-            return minScore;
-        }*/
+        return best;
     }
 }
